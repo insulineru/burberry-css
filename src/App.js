6fc4efec2264e7426542c0a23a3e3d976835b338
insulineru <insulineru@gmail.com>
2020-02-24 15:24:52 +0300

    feat: add base burberry styling for background

--- src/App.js
+++ src/App.js
@@ -1,8 +1,93 @@
 import React from 'react';
+import styled from 'styled-components';
 
 function App() {
+
+  const App = styled.div`
+    height: 100vh;
+    margin: 0;
+    padding: 0;
+    background-color: #ecdab6;
+    background-image: -moz-repeating-linear-gradient(
+        45deg,
+        transparent 5px,
+        rgba(172, 72, 75, 0.5) 5px,
+        rgba(172, 72, 75, 0.5) 11px,
+        rgba(236, 218, 182, 0) 12px,
+        rgba(236, 218, 182, 0) 57px,
+        rgba(54, 50, 51, 0.5) 58px,
+        rgba(54, 50, 51, 0.5) 70px,
+        rgba(255, 255, 255, 0.5) 71px,
+        rgba(255, 255, 255, 0.5) 83px,
+        rgba(54, 50, 51, 0.5) 84px,
+        rgba(54, 50, 51, 0.5) 96px,
+        rgba(255, 255, 255, 0.5) 97px,
+        rgba(255, 255, 255, 0.5) 109px,
+        rgba(54, 50, 51, 0.5) 110px,
+        rgba(54, 50, 51, 0.5) 122px,
+        rgba(236, 218, 182, 0) 123px,
+        rgba(236, 218, 182, 0) 166px
+      ),
+      -moz-repeating-linear-gradient(135deg, transparent 5px,
+        rgba(172, 72, 75, 0.5) 5px,
+        rgba(172, 72, 75, 0.5) 11px,
+        rgba(236, 218, 182, 0) 12px,
+        rgba(236, 218, 182, 0) 57px,
+        rgba(54, 50, 51, 0.5) 58px,
+        rgba(54, 50, 51, 0.5) 70px,
+        rgba(255, 255, 255, 0.5) 71px,
+        rgba(255, 255, 255, 0.5) 83px,
+        rgba(54, 50, 51, 0.5) 84px,
+        rgba(54, 50, 51, 0.5) 96px,
+        rgba(255, 255, 255, 0.5) 97px,
+        rgba(255, 255, 255, 0.5) 109px,
+        rgba(54, 50, 51, 0.5) 110px,
+        rgba(54, 50, 51, 0.5) 122px,
+        rgba(236, 218, 182, 0) 123px,
+        rgba(236, 218, 182, 0)166px);
+
+    background-image: -webkit-repeating-linear-gradient(
+        45deg,
+        transparent 5px,
+        rgba(172, 72, 75, 0.5) 5px,
+        rgba(172, 72, 75, 0.5) 11px,
+        rgba(236, 218, 182, 0) 12px,
+        rgba(236, 218, 182, 0) 57px,
+        rgba(54, 50, 51, 0.5) 58px,
+        rgba(54, 50, 51, 0.5) 70px,
+        rgba(255, 255, 255, 0.5) 71px,
+        rgba(255, 255, 255, 0.5) 83px,
+        rgba(54, 50, 51, 0.5) 84px,
+        rgba(54, 50, 51, 0.5) 96px,
+        rgba(255, 255, 255, 0.5) 97px,
+        rgba(255, 255, 255, 0.5) 109px,
+        rgba(54, 50, 51, 0.5) 110px,
+        rgba(54, 50, 51, 0.5) 122px,
+        rgba(236, 218, 182, 0) 123px,
+        rgba(236, 218, 182, 0) 166px
+      ),
+      -webkit-repeating-linear-gradient(135deg,
+        transparent 5px,
+        rgba(172, 72, 75, 0.5) 5px,
+        rgba(172, 72, 75, 0.5) 11px,
+        rgba(236, 218, 182, 0) 12px,
+        rgba(236, 218, 182, 0) 57px,
+        rgba(54, 50, 51, 0.5) 58px,
+        rgba(54, 50, 51, 0.5) 70px,
+        rgba(255, 255, 255, 0.5) 71px,
+        rgba(255, 255, 255, 0.5) 83px,
+        rgba(54, 50, 51, 0.5) 84px,
+        rgba(54, 50, 51, 0.5) 96px,
+        rgba(255, 255, 255, 0.5) 97px,
+        rgba(255, 255, 255, 0.5) 109px,
+        rgba(54, 50, 51, 0.5) 110px,
+        rgba(54, 50, 51, 0.5) 122px,
+        rgba(236, 218, 182, 0) 123px,
+        rgba(236, 218, 182, 0) 166px);
+  `
+
   return (
-    <div className="App"></div>
+    <App />
   );
 }
 
